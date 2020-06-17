aa394becfd4b2c61bfbfe564d8bb1540141d44be
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-06-17 14:43:32 -0500

    Feat: accessed the id of user and need to pass this id into a DAO method.

--- src/main/java/com/codeup/DadLister/controllers/ViewProfileServlet.java
+++ src/main/java/com/codeup/DadLister/controllers/ViewProfileServlet.java
@@ -1,5 +1,8 @@
 package com.codeup.DadLister.controllers;
 
+import com.codeup.DadLister.dao.DaoFactory;
+import com.codeup.DadLister.models.User;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,18 @@ import java.io.IOException;
 @WebServlet(name = "controllers.ViewProfileServlet", urlPatterns = "/profile")
 public class ViewProfileServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if (request.getSession().getAttribute("user") == null) {
+        //extracted the session from the conditional and placed into its own variable and then use the User object to retrieve the id number which we
+        //will use to add as a parameter to grab the ads from the User
+        User profile = (User) request.getSession().getAttribute("user");
+        if ( profile == null) {
             response.sendRedirect("/login");
             return;
         }
+
+        long profileId = profile.getId();
+
+        //need a dao method to access the database with user ID
+        request.setAttribute("ads", DaoFactory.getAdsDao(). //method acces to DB with (profileId)
         request.getRequestDispatcher("/WEB-INF/profile.jsp").forward(request, response);
     }
 }
